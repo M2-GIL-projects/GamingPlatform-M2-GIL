a57153557b975dfa06c69679c411cddbf6d4acd0
Michel Nassalang <68905963+Michel-Nassalang@users.noreply.github.com>
2024-11-16 10:46:15 +0100

    Update GamingPlatformContext.cs to fix merge

--- GamingPlatform/Data/GamingPlatformContext.cs
+++ GamingPlatform/Data/GamingPlatformContext.cs
@@ -9,11 +9,65 @@ namespace GamingPlatform.Data
 {
     public class GamingPlatformContext : DbContext
     {
-        public GamingPlatformContext (DbContextOptions<GamingPlatformContext> options)
+        public GamingPlatformContext(DbContextOptions<GamingPlatformContext> options)
             : base(options)
         {
         }
 
-        
+       public DbSet<GamingPlatform.Models.User> User { get; set; } = default!;
+        public DbSet<Joueur> LesPlayers { get; set; }
+        public DbSet<Lobby> Lobbies { get; set; }
+        public DbSet<PlayerLobby> PlayerLobbies { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GameType> TypeGames { get; set; }
+        public DbSet<Score> Scores { get; set; }
+        public DbSet<Sentence> Sentences { get; set; }
+
+        // Utilisation d'un DbSet générique pour permettre l'héritage des jeux.
+        public DbSet<SpeedTypingGame> SpeedTypingGames => Set<SpeedTypingGame>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PlayerLobby>()
+                .HasKey(pl => new { pl.JoueurId, pl.LobbyId });
+
+            modelBuilder.Entity<PlayerLobby>()
+                .HasOne(pl => pl.Joueur)
+                .WithMany(u => u.PlayerLobbies)
+                .HasForeignKey(pl => pl.JoueurId);
+
+            modelBuilder.Entity<PlayerLobby>()
+                .HasOne(pl => pl.Lobby)
+                .WithMany(l => l.Players)
+                .HasForeignKey(pl => pl.LobbyId);
+
+            modelBuilder.Entity<Lobby>()
+                .Property(l => l.Code)
+                .IsRequired()
+                .HasMaxLength(6);
+
+            modelBuilder.Entity<Lobby>()
+                .Property(l => l.Name)
+                .IsRequired();
+
+            modelBuilder.Entity<Game>()
+                .HasOne(g => g.Lobby)
+                .WithMany()
+                .HasForeignKey(g => g.LobbyId)
+                .IsRequired(false);  // Ceci rend la relation optionnelle
+
+            modelBuilder.Entity<Game>()
+                .HasOne(g => g.GameType)
+                .WithMany()
+                .HasForeignKey(g => g.GameTypeId);
+
+            // Héritage
+            modelBuilder.Entity<Game>()
+                .HasDiscriminator<string>("GameTypes")
+                .HasValue<SpeedTypingGame>("SpeedTyping")
+                .HasValue<MorpionGame>("Morpion");
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
